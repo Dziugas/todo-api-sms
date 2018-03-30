c6f48ff170365d6d72c8880a25591434e9771535
Dziugas <dziugas.tornau@gmail.com>
2018-03-30 13:03:35 +0300

    SMS detail ...

--- sms/views.py
+++ sms/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+
 from twilio.rest import Client
+
+from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def index(request):
@@ -14,6 +17,15 @@ def index(request):
     number = len(messages)
     return render(request, 'sms/index.html', {'messages' : messages, 'number' : number})
 
+def detail(request, message_id):
+    ACCOUNT_SID = "AC5c35aba82906314a02e40242af329c0b"
+    AUTH_TOKEN = "cd8ce24c9f427e17b92cca8f91bee39c"
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    messages = client.messages.list()
+    return render(request, 'sms/detail.html', {'messages' : messages})
+
+
+
 def delete_sms(request):
     ACCOUNT_SID = "AC5c35aba82906314a02e40242af329c0b"
     AUTH_TOKEN = "cd8ce24c9f427e17b92cca8f91bee39c"
@@ -21,3 +33,15 @@ def delete_sms(request):
     message_id = request.POST.get('message_sid', None)
     client.messages(message_id).delete()
     return redirect('sms:index')
+
+def sms_api(request):
+    ACCOUNT_SID = "AC5c35aba82906314a02e40242af329c0b"
+    AUTH_TOKEN = "cd8ce24c9f427e17b92cca8f91bee39c"
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    messages = client.messages.list()
+    for message in messages:
+        message = client.messages(message.sid).fetch()
+        print(message.body)
+    return JsonResponse(messages, safe=False)
+
+
