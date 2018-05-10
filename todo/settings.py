6878c2e7ca04a8f6adc2bba463dca4c5486c1471
Dziugas <dziugas.tornau@gmail.com>
2018-05-10 10:49:00 +0300

    home page

--- todo/settings.py
+++ todo/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'notes',
     'sms',
+    'home'
 ]
 
 MIDDLEWARE = [
--- todo/urls.py
+++ todo/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     url(r'^notes/', include('notes.urls')),
     url(r'^sms/', include('sms.urls')),
     url(r'^api-auth', include ('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v2/', include((router.urls, 'routers'), namespace='apiv2')),
+    url(r'^notes/api/v2/', include((router.urls, 'routers'), namespace='apiv2')),
 ]
 
