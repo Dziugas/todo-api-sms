72a979776402c903f71228107f93d5e3dbd08204
Dziugas <dziugas.tornau@gmail.com>
2019-01-17 19:08:07 +0200

    UI

--- sms/views.py
+++ sms/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render, redirect
-
+from django.views.decorators.csrf import csrf_exempt
 from twilio.rest import Client
 
-from django.views.decorators.csrf import csrf_exempt
+from todo import twilio_keys
 
-ACCOUNT_SID = "AC4614b91e4db90b0686a0cd932d93b904"
-AUTH_TOKEN = "6a0b6e6378ace0f725335faa92f5409d"
 
 @csrf_exempt
 def index(request):
-
-    # To find these visit https://www.twilio.com/user/account
-
-
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client = Client(twilio_keys.ACCOUNT_SID, twilio_keys.AUTH_TOKEN)
     messages = client.messages.list()
-    number = len(messages)
+    number=0
+    for message in messages:
+        if message.direction=='inbound':
+            number +=1
     return render(request, 'sms/index.html', {'messages' : messages, 'number' : number})
 
 def detail(request, message_sid):
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client = Client(twilio_keys.ACCOUNT_SID, twilio_keys.AUTH_TOKEN)
     message = client.messages(message_sid).fetch()
     return render(request, 'sms/detail.html', {'message' : message})
 
 def delete_sms(request):
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    client = Client(twilio_keys.ACCOUNT_SID, twilio_keys.AUTH_TOKEN)
     message_id = request.POST.get('message_sid', None)
     client.messages(message_id).delete()
     return redirect('sms:index')
